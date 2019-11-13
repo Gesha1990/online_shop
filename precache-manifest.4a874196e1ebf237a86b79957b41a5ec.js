e8c467300fb29c8ffceef690f28fb39d885900a8
Gesha1990 <gesha199019@gmail.com>
2019-11-13 12:38:24 +0200

    Updates

---
+++ precache-manifest.4a874196e1ebf237a86b79957b41a5ec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9dfee36895804a7bc8cc21216a41403",
+    "url": "/online_shop/index.html"
+  },
+  {
+    "revision": "18aec28fa44fd0f58d1e",
+    "url": "/online_shop/static/css/main.bdbb3263.chunk.css"
+  },
+  {
+    "revision": "93fee63baffe46dc339b",
+    "url": "/online_shop/static/js/2.de2038dd.chunk.js"
+  },
+  {
+    "revision": "18aec28fa44fd0f58d1e",
+    "url": "/online_shop/static/js/main.31260f8a.chunk.js"
+  },
+  {
+    "revision": "ca5b9d1db765b830f6c8",
+    "url": "/online_shop/static/js/runtime-main.5ec42832.js"
+  },
+  {
+    "revision": "b34c8640d8657f47292dac20e3a9aff9",
+    "url": "/online_shop/static/media/Spinner-1s-200px.b34c8640.svg"
+  },
+  {
+    "revision": "82ddb728b40fd1a6f71949df95074129",
+    "url": "/online_shop/static/media/add.82ddb728.svg"
+  },
+  {
+    "revision": "47b871b80107e9f8b33d8ee2450373dd",
+    "url": "/online_shop/static/media/article.47b871b8.svg"
+  },
+  {
+    "revision": "9e114e7c37a9f6b1ed0e6c0df0cc7e3b",
+    "url": "/online_shop/static/media/electronics.9e114e7c.png"
+  },
+  {
+    "revision": "71e812dfe1c1a611b2cd3efc988d70db",
+    "url": "/online_shop/static/media/login.71e812df.svg"
+  },
+  {
+    "revision": "9956934db0cafaa12f0c3336f92b5319",
+    "url": "/online_shop/static/media/logout.9956934d.svg"
+  }
+]);
